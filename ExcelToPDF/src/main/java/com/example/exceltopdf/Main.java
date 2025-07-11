e5cd9136e77348a1b24c1aff783de63921ab0388
BuYehui <905739651@qq.com>
2025-07-11 11:10:10 +0800

    Excel文件添加分页符修正第一页空白内容挤到第二页的问题

--- ExcelToPDF/src/main/java/com/example/exceltopdf/Main.java
+++ ExcelToPDF/src/main/java/com/example/exceltopdf/Main.java
@@ -1,18 +1,10 @@
 package com.example.exceltopdf;
 
-import com.example.controller.MyController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.rendering.PDFRenderer;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 public class Main extends Application {
@@ -23,7 +15,6 @@ public class Main extends Application {
         loader.setLocation(getClass().getResource("/window.fxml"));
         Pane pane = loader.load();
         Scene scene = new Scene(pane);
-        MyController controller = loader.getController();
         stage.setTitle("Excel预览");
         stage.setScene(scene);
         stage.setResizable(false);
